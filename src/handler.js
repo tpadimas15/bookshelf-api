41c287d5a6aa0006110c925f628421e73dac595b
tpadimas <adimasputra799@gmail.com>
2022-04-04 21:16:42 +0700

    fix bug in editBookByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -157,9 +157,27 @@ const editBookByIdHandler = (request, h) => {
   const { bookId } = request.params;
 
   const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+  const finished = pageCount === readPage;
   const updatedAt = new Date().toISOString();
 
   const index = books.findIndex((book) => book.id === bookId);
+
+  if (name === undefined) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal memperbarui buku. Mohon isi nama buku",
+    });
+    response.code(400);
+    return response;
+  }
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+    });
+    response.code(400);
+    return response;
+  }
 
   if (index !== -1) {
     books[index] = {
@@ -172,6 +190,7 @@ const editBookByIdHandler = (request, h) => {
       pageCount,
       readPage,
       reading,
+      finished,
       updatedAt,
     };
     const response = h.response({
@@ -179,21 +198,6 @@ const editBookByIdHandler = (request, h) => {
       message: "Buku berhasil diperbarui",
     });
     response.code(200);
-    return response;
-  }
-  if (name === undefined) {
-    const response = h.response({
-      status: "fail",
-      message: "Gagal memperbarui buku. Mohon isi nama buku",
-    });
-    response.code(400);
-    return response;
-  } else if (readPage > pageCount) {
-    const response = h.response({
-      status: "fail",
-      message: "Gagal memperbarui. readPage tidak boleh lebih besar dari pageCount",
-    });
-    response.code(400);
     return response;
   } else {
     const response = h.response({
